63d11227681124194f42e1d02d22233acd29d75d
vikarmaulanaarrisyad <vikar.maulana.arrisyad@gmail.com>
2022-12-09 09:53:31 +0700

    menambahkan template adminlte versi 3 & laravel jetstream

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,13 @@ Route::get('/', function () {
 Route::group(['prefix' => 'admin'],  function () {
     Route::get('dashboard', [DashboardController::class, 'index']);
 });
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified'
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
